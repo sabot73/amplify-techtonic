dd49a986a5982f51d4d264af577e080b33436a5d
info@saintblock.com <info@saintblock.com>
2020-05-04 19:19:26 +0900

    add test

--- src/App.js
+++ src/App.js
@@ -9,6 +9,8 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+
+          Hello from AWS Amplify ^^
         </p>
         <a
           className="App-link"
